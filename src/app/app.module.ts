1cd0afee8caebaf6a260e9e1552f93fae79442a2
Frank <francois.matta@designer-24.com>
2023-02-19 11:08:45 +0200

    finished form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatSelectModule} from '@angular/material/select'
 import {MatButtonModule} from '@angular/material/button';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 const Materials = [
   MatFormFieldModule,
@@ -21,7 +22,8 @@ const Materials = [
   MatAutocompleteModule,
   MatSelectModule,
   MatButtonModule,
-  MatDatepickerModule
+  MatDatepickerModule,
+  MatNativeDateModule
 ]
 
 @NgModule({
--- src/app/guest-details/guest-details.component.ts
+++ src/app/guest-details/guest-details.component.ts
@@ -1,40 +1,72 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormBuilder, Validators, NgForm, FormControl, FormGroupDirective } from '@angular/forms';
+import {
+  FormBuilder,
+  Validators,
+  NgForm,
+  FormControl,
+  FormGroupDirective,
+  FormGroup,
+} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-import countriesList from '../../assets/countries.json'
-
+import countriesList from '../../assets/countries.json';
 interface Country {
-  name: string,
-  code: string
+  name: string;
+  code: string;
 }
 
 @Component({
   selector: 'app-guest-details',
   templateUrl: './guest-details.component.html',
-  styleUrls: ['./guest-details.component.scss']
+  styleUrls: ['./guest-details.component.scss'],
 })
 export class GuestDetailsComponent {
   countries: Country[] = countriesList;
   matcher = new MyErrorStateMatcher();
-  minDate: Date = new Date();
-  maxDate: Date = new Date();
-  @ViewChild('guestDetailsFormDirective') guestDetailsFormDirective! : NgForm
+  minDate: Date;
+  maxDate: Date;
+  @ViewChild('guestDetailsFormDirective') guestDetailsFormDirective!: NgForm;
 
   guestDetails = this.formBuilder.group({
-    firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-    lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-    city: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+    firstName: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(50)],
+    ],
+    lastName: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(50)],
+    ],
+    city: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(50)],
+    ],
     country: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
     dateOfBirth: ['', [Validators.required]],
     passportDetails: this.formBuilder.group({
-      passportNumber: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(9)]],
-      foreName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-      surName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      passportNumber: [
+        null,
+        [Validators.required, Validators.minLength(9), Validators.maxLength(9)],
+      ],
+      foreName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ],
+      ],
+      surName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ],
+      ],
       dateOfIssue: ['', Validators.required],
-      dateOfExpiry: ['', Validators.required]
-    })
-  })
+      dateOfExpiry: ['', Validators.required],
+    }),
+  });
 
   get firstName() {
     return this.guestDetails.get('firstName');
@@ -60,13 +92,43 @@ export class GuestDetailsComponent {
     return this.guestDetails.get('dateOfBirth');
   }
 
-  constructor(private formBuilder: FormBuilder) {}
-  
+  get passportDetails() {
+    return this.guestDetails.get('passportDetails') as FormGroup;
+  }
+
+  get passportNumber() {
+    return this.passportDetails.get('passportNumber');
+  }
+
+  get foreName() {
+    return this.passportDetails.get('foreName');
+  }
+
+  get surName() {
+    return this.passportDetails.get('surName');
+  }
+
+  get dateOfIssue() {
+    return this.passportDetails.get('dateOfIssue');
+  }
+
+  get dateOfExpiry() {
+    return this.passportDetails.get('dateOfExpiry');
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 100, 0, 1);
+    this.maxDate = new Date(currentYear - 18, 11, 31);
+  }
+
   onSubmit() {
     console.log(this.guestDetails.value);
+    console.log(
+      this.guestDetails.get('passportDetails')?.get('passportNumber')?.errors
+    );
   }
 }
-
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
